d06bb075649ad2566c5dc796c8b7ee848cf2685f
Jiwon <vnack1@gmail.com>
2025-09-17 16:03:54 +0900

    update

--- server/src/routes/notices.routes.js
+++ server/src/routes/notices.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as ctrl from "../controllers/notices.controller.js";
+import { requireAuth, requireAdmin } from "../middlewares/auth.js";
 
 const r = Router();
 r.get("/", ctrl.list); //리스트
 r.get("/:id", ctrl.detail); //상세
-r.post("/", ctrl.create); //생성
-r.patch("/:id", ctrl.update); //수정
-r.delete("/:id", ctrl.remove); //삭제
+r.post("/", requireAuth, requireAdmin, ctrl.create); //생성
+r.patch("/:id", requireAuth, requireAdmin, ctrl.update); //수정
+r.delete("/:id", requireAuth, requireAdmin, ctrl.remove); //삭제
 export default r;
